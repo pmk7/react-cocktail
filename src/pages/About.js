32e49956cc0a351ffa7a065c7f44088e64863849
pmk7 <72909830+pmk7@users.noreply.github.com>
2021-07-21 18:21:59 +0200

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section-about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        pariatur at veritatis praesentium quae? Nemo veniam ratione fugit a
+        quisquam modi labore corrupti amet unde laudantium. Delectus nam
+        possimus veritatis!
+      </p>
+    </section>
+  );
+};
+
+export default About;
